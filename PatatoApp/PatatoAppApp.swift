6913999bb606a91a7ec145eb29c978c894dc6cd3
Shciceq <147521@scwsd.us>
2025-05-09 08:15:48 -0400

    Initial Commit

---
+++ PatatoApp/PatatoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatatoAppApp.swift
+//  PatatoApp
+//
+//  Created by Bohdan Harbuziuk on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PatatoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PatatoAppTests/PatatoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PatatoAppTests.swift
+//  PatatoAppTests
+//
+//  Created by Bohdan Harbuziuk on 5/9/25.
+//
+
+import Testing
+@testable import PatatoApp
+
+struct PatatoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
